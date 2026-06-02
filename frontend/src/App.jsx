c7f1f68ae7c0b4ce83e099b517f03f847bdd41fd
SeungwooDY <sdyoon0701@gmail.com>
2026-06-02 10:01:26 -0400

    navbar rebased with main and uses react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,11 @@
 import { NavLink, Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <NavLink to="/">Landing</NavLink>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/recipes">Recipes</NavLink>
-        <NavLink to="/detail">Recipe Details</NavLink>
-        <NavLink to="/create">Create Recipe</NavLink>
-        <NavLink to="/myrecipes">My Recipes</NavLink>
-        <NavLink to="/admin">Admin Dashboard</NavLink>
-
-        <Outlet />
-      </nav>
+      <Navbar />
+      <Outlet />
     </>
   )
 }
